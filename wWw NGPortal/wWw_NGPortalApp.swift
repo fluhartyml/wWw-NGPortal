1713f53cd64354e1ff9556a4ac9c02690cfe8dd0
Michael Fluharty <michael.fluharty@mac.com>
2025-10-31 11:28:21 -0500

    Initial Commit

---
+++ wWw NGPortal/wWw_NGPortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  wWw_NGPortalApp.swift
+//  wWw NGPortal
+//
+//  Created by Michael Fluharty on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct wWw_NGPortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
